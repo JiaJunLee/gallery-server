3df499ed50d6d34f7573b240d67caea988a9158c
lance <minelhjun@gmail.com>
2019-03-10 16:39:51 +0800

    add ImageServiceTest.groovy test

---
+++ src/test/groovy/com/oocl/ita/gallery/image/ImageServiceTest.groovy
@@ -0,0 +1,22 @@
+package com.oocl.ita.gallery.image
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class ImageServiceTest{
+    @Autowired
+    ImageService imageService
+    @Test
+    void should_return_not_null_when_get_repository(){
+        Assert.assertNotNull(imageService.getRepository())
+    }
+
+}
+
+
