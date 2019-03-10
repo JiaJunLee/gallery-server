4014969e812da887e4c6c099f5bf738c4da94587
Thomas Chen <thomas.y.chen@oocl.com>
2019-03-10 22:23:39 +0800

    modified user Register

---
+++ src/main/groovy/com/oocl/ita/gallery/common/constants/ErrorMsgConstants.groovy
@@ -0,0 +1,11 @@
+package com.oocl.ita.gallery.common.constants
+
+class ErrorMsgConstants {
+
+    protected ErrorMsgConstants() {
+
+    }
+
+    /* User */
+    static final String REG_FAIL_CONFLICT_USERNAME = "username has been registered";
+}
