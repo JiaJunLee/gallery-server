c95d98f9f1db586c24bc45d87c4d2421e55def33
libu <burgess.li@oocl.com>
2019-03-10 10:56:40 +0800

    initialize project

---
+++ src/main/groovy/com/oocl/ita/gallery/GalleryApplication.groovy
@@ -0,0 +1,13 @@
+package com.oocl.ita.gallery
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GalleryApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GalleryApplication, args)
+	}
+
+}
