cb70e0762643cf355d48e0ef52afe9c3552442b3
lance <minelhjun@gmail.com>
2019-03-10 15:10:33 +0800

    update release

--- src/main/groovy/com/oocl/ita/gallery/GalleryApplication.groovy
+++ src/main/groovy/com/oocl/ita/gallery/GalleryApplication.groovy
@@ -2,12 +2,18 @@ package com.oocl.ita.gallery
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class GalleryApplication {
+class GalleryApplication extends SpringBootServletInitializer{
 
 	static void main(String[] args) {
 		SpringApplication.run(GalleryApplication, args)
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GalleryApplication.class);
+    }
 }
