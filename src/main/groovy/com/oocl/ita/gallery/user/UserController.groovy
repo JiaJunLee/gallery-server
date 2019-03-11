73578ac4b7df502aa8f0eb8bdf0d63847018c4be
Thomas Chen <thomas.y.chen@oocl.com>
2019-03-11 10:14:48 +0800

    add user login

--- src/main/groovy/com/oocl/ita/gallery/user/UserController.groovy
+++ src/main/groovy/com/oocl/ita/gallery/user/UserController.groovy
@@ -1,6 +1,10 @@
 package com.oocl.ita.gallery.user
 
+import com.oocl.ita.gallery.common.constants.ErrorMsgConstants
+import com.oocl.ita.gallery.common.constants.MsgConstants
+import com.oocl.ita.gallery.security.AuthenticationException
 import com.oocl.ita.gallery.security.utils.HMAC
+import com.oocl.ita.gallery.security.utils.JWT
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -9,12 +13,23 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
+
 @Controller
 @RequestMapping('/user')
 class UserController {
 
     @Autowired
+    JWT jwt
+
+    @Autowired
     UserService userService
+
+    @RequestMapping('/denied')
+    ResponseEntity denied() {
+        return new ResponseEntity<String>(ErrorMsgConstants.AUTH_DENIED, HttpStatus.UNAUTHORIZED)
+    }
 
     @PostMapping("/register")
     ResponseEntity register(@RequestBody User user) {
@@ -27,6 +42,28 @@ class UserController {
                 password: HMAC.digest(user.password, hsKey, HMAC.HMAC_SHA512),
                 type: User.UserType.PUBLIC
         )
-        return new ResponseEntity(userService.createUser(userDB), HttpStatus.CREATED);
+        return new ResponseEntity<User>(userService.createUser(userDB), HttpStatus.CREATED)
+    }
+
+    @PostMapping("/login")
+    ResponseEntity login(@RequestBody User user, HttpServletResponse httpServletResponse) {
+        User userDB = userService.findByUsername(user.username)
+        if (userDB == null) {
+            throw new AuthenticationException(ErrorMsgConstants.AUTH_NOT_FOUND_USER)
+        }
+
+        if (!HMAC.validate(user.password, user?.hsKey, HMAC.HMAC_SHA512, user?.password)) {
+            throw new AuthenticationException(ErrorMsgConstants.AUTH_FAIL)
+        }
+
+        String jwtToken = jwt?.sign(['id': userDB?.id, 'username': userDB?.username, 'hsKey': userDB?.hsKey, 'hsPassword': userDB?.password])
+
+        Cookie cookie = new Cookie('token', jwtToken)
+        cookie.setHttpOnly(true)
+        cookie.setPath('/')
+        cookie.setMaxAge(JWT.TOKEN_EXPIRE_TIME)
+        httpServletResponse.addCookie(cookie)
+
+        return new ResponseEntity<String>(MsgConstants.USER_LOGIN, HttpStatus.OK)
     }
 }
