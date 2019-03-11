cc3c99d3cdd248366e60edd82953dcfacf646a35
Thomas Chen <thomas.y.chen@oocl.com>
2019-03-11 11:05:31 +0800

    add UserServiceTest

--- src/test/groovy/com/oocl/ita/gallery/user/UserServiceTest.groovy
+++ src/test/groovy/com/oocl/ita/gallery/user/UserServiceTest.groovy
@@ -1,20 +1,101 @@
 package com.oocl.ita.gallery.user
 
-import io.jsonwebtoken.lang.Assert
+import com.oocl.ita.gallery.common.BaseService
+import com.oocl.ita.gallery.user.exception.DuplicatedUserException
+import org.assertj.core.util.Lists
+import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.powermock.api.support.membermodification.MemberMatcher
 
-@RunWith(SpringRunner)
-@SpringBootTest
+import static org.powermock.api.support.membermodification.MemberModifier.suppress
+
 class UserServiceTest {
-    @Autowired
-    UserService userService
+
+    @InjectMocks
+    private UserService userService = new UserService();
+
+    @Mock
+    private UserRepository userRepository;
+
+
+    @Before
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(userRepository.findAllByType(Mockito.eq(User.UserType.PUBLIC))).thenReturn(Lists.newArrayList())
+        Mockito.when(userRepository.findByUsername(Mockito.eq("b"))).thenReturn(new User())
+        Mockito.when(userRepository.findByUsername(Mockito.eq("a"))).thenReturn(null)
+    }
 
     @Test
-    void should_return_user_when_findAllByType_given_user_type_administrator() {
-        Assert.notNull(userService.findAllByType(User.UserType.ADMINISTRATOR))
+    void should_return_userRepository_when_getRepository() {
+        //When
+        UserRepository userRepository1 = userService.getRepository();
+
+        //Then
+        Assert.assertNotNull(userRepository1)
+    }
+
+
+    @Test
+    void should_return_empty_user_list_when_findAllByType_given_user_type_public() {
+        //When
+        List<User> userList = userService.findAllByType(User.UserType.PUBLIC)
+
+        //Then
+        Assert.assertEquals(0, userList.size())
+    }
+
+    @Test
+    void should_return_user_null_when_findByUsername_given_user_name_a() {
+        //Given
+        String username = "a"
+
+        //When
+        User user = userService.findByUsername(username)
+
+        //Then
+        Assert.assertNull(user)
+    }
+
+    @Test
+    void should_return_user_not_null_when_findByUsername_given_user_name_b() {
+        //Given
+        String username = "b"
+
+        //When
+        User user = userService.findByUsername(username)
+
+        //Then
+        Assert.assertNotNull(user)
+    }
+
+    @Test
+    public void should_return_user_not_null_when_createuser_given_user_name_a() {
+        //Given
+        suppress(MemberMatcher.methodsDeclaredIn(BaseService.class))
+        User user = new User(username: "a", nickname: "A", gender: 1, hsKey: "key",
+                password: "pwd", type: User.UserType.PUBLIC)
+
+        //When
+        User userDB = userService.createUser(user)
+
+        //Then
+        Assert.assertNotNull(userDB)
+    }
+
+    @Test(expected = DuplicatedUserException.class)
+    public void should_catch_DuplicatedUserException_when_create_given_user_name_b() {
+        //Given
+        suppress(MemberMatcher.methodsDeclaredIn(BaseService.class))
+        User user = new User(username: "b", nickname: "B", gender: 1, hsKey: "key",
+                password: "pwd", type: User.UserType.PUBLIC)
+
+        //When
+        User userDB = userService.createUser(user)
     }
 }
