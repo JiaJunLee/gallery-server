5c9de8dc9aaf726d73d79bdfdbf09a1e4d92c6da
Thomas Chen <thomas.y.chen@oocl.com>
2019-03-13 10:52:23 +0800

    add unit test

--- src/test/groovy/com/oocl/ita/gallery/security/SmartRealmTest.groovy
+++ src/test/groovy/com/oocl/ita/gallery/security/SmartRealmTest.groovy
@@ -2,91 +2,119 @@ package com.oocl.ita.gallery.security
 
 import com.oocl.ita.gallery.common.constants.ErrorMsgConstants
 import com.oocl.ita.gallery.security.utils.JWT
+import com.oocl.ita.gallery.user.User
 import com.oocl.ita.gallery.user.UserService
+import org.apache.shiro.authc.AuthenticationInfo
 import org.apache.shiro.authc.AuthenticationToken
 import org.apache.shiro.authc.UsernamePasswordToken
 import org.apache.shiro.authz.AuthorizationInfo
-import org.junit.*
-import org.junit.rules.ExpectedException
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
+import spock.lang.Specification
 
-class SmartRealmTest {
+class SmartRealmTest extends Specification {
 
-    @InjectMocks
-    private SmartRealm smartRealm = new SmartRealm()
+    SmartRealm smartRealm
+    UserService userService
+    JWT jwt
 
-//    @Mock
-//    private JWT jwt
 
-    @Mock
-    private UserService userService
-
-    @Rule
-    public ExpectedException thrown= ExpectedException.none();
-
-    @Before
-    void setUp() {
-        MockitoAnnotations.initMocks(this)
-        //Mockito.when(jwt.getUsername(Mockito.eq("abc"))).thenReturn(null)
+    def setup() {
+        userService = Mock(UserService)
+        jwt = Mock(JWT)
+        smartRealm = new SmartRealm(userService: userService, jwt: jwt)
     }
 
 
-    @Test
-    void should_return_name_when_getName() {
-        //When
-        String name = smartRealm.getName()
+    def 'should return AUTH_TOKEN_UNRECOGNIZED when doGetAuthenticationInfo given no username token'() {
+        given:
+        jwt.getUsername(_) >> ""
+        Token token = new Token(token: "")
 
-        //Then
-        Assert.assertNotNull(name)
-        Assert.assertEquals("SMART-REALM",name)
-    }
-
-    @Test
-    void should_return_true_when_supports_given_AuthenticationToken() {
-        //Given
-        Token token = new Token()
-
-        //When
-        boolean supports = smartRealm.supports(token)
-
-        //Then
-        Assert.assertTrue(supports)
-    }
-
-    @Test
-    void should_return_false_when_supports_given_AuthenticationToken() {
-        //Given
-        AuthenticationToken token = new UsernamePasswordToken()
-
-        //When
-        boolean supports = smartRealm.supports(token)
-
-        //Then
-        Assert.assertFalse(supports)
-    }
-
-    @Test
-    void should_return_null_when_doGetAuthorizationInfo() {
-        //When
-        AuthorizationInfo info = smartRealm.doGetAuthorizationInfo(null)
-
-        //Then
-        Assert.assertNull(info)
-    }
-
-    @Test(expected = org.apache.shiro.authc.AuthenticationException.class)
-    void should_throw_token_AUTH_TOKEN_UNRECOGNIZED_when_doGetAuthenticationInfo_given_token_abc() {
-        //Given
-        String tokenStr = "abc"
-        Token token = new Token(token: tokenStr)
-
-        //When
+        when:
         smartRealm.doGetAuthenticationInfo(token)
 
-        //Then
-        //thrown.expect(AuthenticationException.class)
-        //thrown.expectMessage(ErrorMsgConstants.AUTH_TOKEN_UNRECOGNIZED)
+        then:
+        def ex = thrown(Exception)
+        ex.getMessage() == ErrorMsgConstants.AUTH_TOKEN_UNRECOGNIZED
+    }
+
+    def 'should return AUTH_NOT_FOUND_USER when doGetAuthenticationInfo given username no in DB'() {
+        given:
+        jwt.getUsername(_) >> "a"
+        userService.findByUsername(_) >> null
+        Token token = new Token(token: "")
+
+        when:
+        smartRealm.doGetAuthenticationInfo(token)
+
+        then:
+        def ex = thrown(Exception)
+        ex.getMessage() == ErrorMsgConstants.AUTH_NOT_FOUND_USER
+    }
+
+    def 'should return AUTH_FAIL when doGetAuthenticationInfo given token is incorrect'() {
+        given:
+        jwt.getUsername(_) >> "a"
+        jwt.verify(_, _) >> false
+        userService.findByUsername(_) >> new User()
+        Token token = new Token(token: "")
+
+        when:
+        smartRealm.doGetAuthenticationInfo(token)
+
+        then:
+        def ex = thrown(Exception)
+        ex.getMessage() == ErrorMsgConstants.AUTH_FAIL
+    }
+
+    def 'should return SimpleAuthenticationInfo when doGetAuthenticationInfo given token is correct'() {
+        given:
+        jwt.getUsername(_) >> "a"
+        jwt.verify(_, _) >> true
+        userService.findByUsername(_) >> new User()
+        Token token = new Token(token: "")
+
+        when:
+        AuthenticationInfo info = smartRealm.doGetAuthenticationInfo(token)
+
+        then:
+        info != null
+    }
+
+    def 'should return SMART-REALM when getName'() {
+        when:
+        String name = smartRealm.getName()
+
+        then:
+        name != null
+        name == "SMART-REALM"
+    }
+
+    def 'should return null when doGetAuthorizationInfo'() {
+        when:
+        AuthorizationInfo info = smartRealm.doGetAuthorizationInfo()
+
+        then:
+        info == null
+    }
+
+    def 'should return false when supports given UsernamePasswordToken'() {
+        given:
+        AuthenticationToken token = new UsernamePasswordToken()
+
+        when:
+        boolean result = smartRealm.supports(token)
+
+        then:
+        result == false
+    }
+
+    def 'should return #result when supports given #token'() {
+
+        expect:
+        smartRealm.supports(token) == result
+
+        where:
+        token                       | result
+        new Token()                 | true
     }
 }
